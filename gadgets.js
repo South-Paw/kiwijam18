bd4f990a9c2a2fc2eda07e3e6dbaffab3b482d5a
Alex Gabites <alex@netspeed.net.nz>
2018-07-22 10:54:40 +1200

    format

--- gadgets.js
+++ gadgets.js
@@ -1,59 +1,65 @@
 function makeBrazier(pos, lit = false) {
-	let age = 0;
+  let age = 0;
   let frame = 0;
   let {
     getPos,
-		setPos,
+    setPos,
   } = makeEntity(pos);
 
-	function flicker(a) {
-		return (Math.sin(age/100)+Math.sin(age/180))/3;
-	}
+  function flicker(a) {
+    return (Math.sin(age / 100) + Math.sin(age / 180)) / 3;
+  }
 
   function draw(ctx, lctx) {
     let [x, y] = getPos();
 
-		if (lit) {
-			ctx.drawSprite(Assets.brazierBurn, x, y, randInt(3));
-			let flip=1;
-			if (probability(0.5)) flip=-flip;
-    	lctx.drawSprite(Assets.baseLight, x, y, randInt(8),(7+flicker(age))*flip);
-		} else {
-			ctx.drawSprite(Assets.brazierEmber, x, y, randInt(6));
-		}
+    if (lit) {
+      ctx.drawSprite(Assets.brazierBurn, x, y, randInt(3));
+
+      let flip = 1;
+
+      if (probability(0.5)) {
+        flip = -flip
+      };
+
+      lctx.drawSprite(Assets.baseLight, x, y, randInt(8), (7 + flicker(age)) * flip);
+    } else {
+      ctx.drawSprite(Assets.brazierEmber, x, y, randInt(6));
+    }
   }
 
   function move() {
     let p = getPos();
-		age += 1;
-		if (world.player.hasFire()) {
-			if (vdistance(p,world.player.getPos()) < 100) {
-				lit=true;
-			}
-		}
+    age += 1;
+    if (world.player.hasFire()) {
+      if (vdistance(p, world.player.getPos()) < 100) {
+        lit = true;
+      }
+    }
   }
-	function blocking(atPos) {
-		let d = vdistance(atPos,getPos());
 
-		return d<40;
-	}
+  function blocking(atPos) {
+    let d = vdistance(atPos, getPos());
 
-	let result= {
+    return d < 40;
+  }
+
+  let result = {
     getPos,
     setPos,
-		move,
-		blocking,
+    move,
+    blocking,
     draw
-	};
+  };
 
-	let [tx,ty]=gameToTile(pos);
+  let [tx, ty] = gameToTile(pos);
 
-	//this is bad don't blindly put in 9 tiles,  fix when not tired
-	for (let x=-1; x<2;x++) {
-		for (let y=-1; y<2;y++) {
-			world.tileAt([tx+x,ty+y]).contents.push(result);
-		}
-	}
+  //this is bad don't blindly put in 9 tiles,  fix when not tired
+  for (let x = -1; x < 2; x++) {
+    for (let y = -1; y < 2; y++) {
+      world.tileAt([tx + x, ty + y]).contents.push(result);
+    }
+  }
 
   return result;
 }
@@ -64,8 +70,8 @@ function makeRat(pos) {
   let frame = 0;
   let {
     getPos,
-		setPos,
-		blocking
+    setPos,
+    blocking
   } = makeEntity(pos)
 
   function draw(ctx, lctx) {
@@ -83,8 +89,8 @@ function makeRat(pos) {
   return {
     getPos,
     setPos,
-		move,
-		blocking,
+    move,
+    blocking,
     draw
   };
 }
@@ -92,16 +98,16 @@ function makeRat(pos) {
 
 function makeKey(pos) {
   let age = 0;
-	let frame = 0;
-	let collected = false;
+  let frame = 0;
+  let collected = false;
   let {
     getPos,
-		setPos,
-		blocking
+    setPos,
+    blocking
   } = makeEntity(pos)
 
   function draw(ctx, lctx) {
-		if (collected) return
+    if (collected) return
     let [x, y] = getPos();
 
     ctx.drawSprite(Assets.key, x, y);
@@ -109,21 +115,21 @@ function makeKey(pos) {
   }
 
   function move() {
-		if (collected) return;
-		let p = getPos();
-		
+    if (collected) return;
+
+    let p = getPos();
+
     age += 1;
 
-		if (vdistance(p,world.player.getPos()) < 64) {
-			collected = true;
-			
-		}		
+    if (vdistance(p, world.player.getPos()) < 64) {
+      collected = true;
+    }
   }
   return {
     getPos,
     setPos,
-		move,
-		blocking,
+    move,
+    blocking,
     draw
   };
 }

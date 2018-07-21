0495f9df6ef9415d4b288ec8c9c623c4b4903e2d
Alex Gabites <alex@netspeed.net.nz>
2018-07-21 17:28:54 +1200

    format entities

--- entities.js
+++ entities.js
@@ -3,12 +3,14 @@ function makeEntity(pos) {
   let setPos = newpos => pos = newpos;
   let move = _ => {};
   let draw = _ => {};
-	let blocking = (x,y)=>{false};
-	
+  let blocking = (x, y) => {
+    false
+  };
+
   return {
     getPos,
-		setPos,
-		blocking,
+    setPos,
+    blocking,
     move,
     draw
   };
@@ -17,24 +19,24 @@ function makeEntity(pos) {
 
 function makeMinotaur(pos) {
   let WALKING_SPEED = 5;
-	let age=0;
-	let frame=0;
-	let anim=Assets.minotaurWalkLeft;
+  let age = 0;
+  let frame = 0;
+  let anim = Assets.minotaurWalkLeft;
   let {
     getPos,
     setPos
-	} = makeEntity(pos)
+  } = makeEntity(pos)
 
-	function draw(ctx,lctx) {
-		let [x, y] = getPos();
-		ctx.drawSprite(anim,x,y,frame);
-	}
+  function draw(ctx, lctx) {
+    let [x, y] = getPos();
+    ctx.drawSprite(anim, x, y, frame);
+  }
 
-	function move() {
-		age+=1;
-		frame=Math.floor((age/10) % 6);
+  function move() {
+    age += 1;
+    frame = Math.floor((age / 10) % 6);
 
-	}
+  }
   return {
     getPos,
     setPos,
@@ -45,61 +47,61 @@ function makeMinotaur(pos) {
 
 function makePlayer(pos) {
   let WALKING_SPEED = 5;
-	let walkAnim=Assets.playerWalkLeft;
-	let age=0;
-	let frame=0;
+  let walkAnim = Assets.playerWalkLeft;
+  let age = 0;
+  let frame = 0;
   let {
     getPos,
     setPos
   } = makeEntity(pos)
 
   function draw(ctx, lctx) {
-  	let [x, y] = getPos();
-		
-		ctx.drawSprite(walkAnim,x,y,frame);
-	
-		lctx.drawSprite(Assets.baseLight, x, y-32, randInt(8));
+    let [x, y] = getPos();
+
+    ctx.drawSprite(walkAnim, x, y, frame);
+
+    lctx.drawSprite(Assets.baseLight, x, y - 32, randInt(8));
 
   }
 
   function move() {
     let p = getPos();
-		age+=1;
-		let walking=false;
+    age += 1;
+    let walking = false;
 
-		
+
     // Move up
     if (input.isKeyDown(87)) {
-			p = vadd([0, -WALKING_SPEED], p);
-			walkAnim=Assets.playerWalkUp;
-			walking=true;
+      p = vadd([0, -WALKING_SPEED], p);
+      walkAnim = Assets.playerWalkUp;
+      walking = true;
     }
 
     // Move down
     if (input.isKeyDown(83)) {
-			p = vadd([0, WALKING_SPEED], p);
-			walkAnim=Assets.playerWalkDown;
-			walking=true;
+      p = vadd([0, WALKING_SPEED], p);
+      walkAnim = Assets.playerWalkDown;
+      walking = true;
     }
 
     // Move left
     if (input.isKeyDown(65)) {
-			p = vadd([-WALKING_SPEED, 0], p);
-			walkAnim=Assets.playerWalkLeft;
-			walking=true;
+      p = vadd([-WALKING_SPEED, 0], p);
+      walkAnim = Assets.playerWalkLeft;
+      walking = true;
     }
 
     // Move right
     if (input.isKeyDown(68)) {
-			p = vadd([WALKING_SPEED, 0], p);
-			walkAnim=Assets.playerWalkRight;
-			walking=true;
-		
+      p = vadd([WALKING_SPEED, 0], p);
+      walkAnim = Assets.playerWalkRight;
+      walking = true;
     }
 
-		if (walking) {
-			frame = Math.floor((age/10) % 4);
-		}
+    if (walking) {
+      frame = Math.floor((age / 10) % 4);
+    }
+
     setPos(p);
   }
 
